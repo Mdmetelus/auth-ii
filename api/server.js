4dc3d1a9be02b88758d653ee559f1b51f69be82f
Mr. Metelus <Mdmetelus@gmail.com>
2019-01-23 17:11:29 -0500

    generate token created

--- api/server.js
+++ api/server.js
@@ -5,23 +5,51 @@ const helmet = require ('helmet');
 const knex = require ('knex');
 const bcrypt = require ('bcryptjs'); // added
 const jwt = require ('jsonwebtoken');
+
+const knexConfig = require('../knexfile.js');
+
 const server = express ();
+
+
+const db = knex(knexConfig.development);
 
 server.use (express.json ());
 server.use (helmet ());
 
 //routes
 server.get ('/', (req, res) => {
-  res.send (`API working.\n Sanity Check\n Test Route!`);
+  res.send (`API working.\n CheckRoute\n Test Route!`);
 });
 
 //Endpoints
 server.post ('/api/register', (req, res) => {
-  const blank = req.body;
+  const userInfo = req.body;
 
   const hash = bcrypt.hashSync (userInfo.password, 16);
 
-  blank.password = hash;
+  userInfo.password = hash;
+  db('users').insert(userInfo).then( ids => {
+    res.status(201).json(ids);
+  }).catch(err => res.status(500).json(err));
+  
 });
+
+
+function generateUserToken(user) {
+  const payload = {
+    username: user.username,
+    password: user.pasword,
+    department:user.department,
+  };
+
+  const secret = process.env.JWT_SECRET;
+
+  const options= {
+    expiresIn: '60m',
+
+  }
+
+  return jwt.sign(payload,secret, options);
+}
 
 module.exports = server;
