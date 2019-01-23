682f54925d02bc36beb59e04d9fd21ad54e0e820
Mr. Metelus <Mdmetelus@gmail.com>
2019-01-23 15:43:50 -0500

    ".env, knexfile, and some moredependencies added"

--- api/server.js
+++ api/server.js
@@ -1,5 +1,10 @@
+require ('dotenv').config ();
+
 const express = require ('express');
 const helmet = require ('helmet');
+const knex = require ('knex');
+const bcrypt = require ('bcryptjs'); // added
+const jwt = require ('jsonwebtoken');
 const server = express ();
 
 server.use (express.json ());
