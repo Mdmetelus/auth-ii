ca9f4a15d5d776123e457e5380c52ac23250f8bd
Mr. Metelus <Mdmetelus@gmail.com>
2019-01-23 18:45:28 -0500

    "update"

---
+++ client/src/users/Users.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+
+class Users extends Component {
+  render() {
+    return (
+      <div className="Users">
+        
+      </div>
+    );
+  }
+}
+
+export default Users;
